377640e5d1edc2da653b9b78c14002bbd978db10
Miguel <miguel_ilutec@hotmail.com>
2021-06-23 10:40:33 +0200

    commit

--- models/Concert.js
+++ models/Concert.js
@@ -26,6 +26,7 @@ const ConcertSchema = mongoose.Schema({
   },
   imageUrl: {
     type: String,
+    default: 'https://images.pexels.com/photos/1763075/pexels-photo-1763075.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
   },
   images:[{ type: String }],
 });
