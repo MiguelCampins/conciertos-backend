6a05b2f182c16c08f28726e1a0cacc9ea9db7d14
Miguel <miguel_ilutec@hotmail.com>
2022-06-13 23:23:24 +0200

    rutas * v18

--- index.js
+++ index.js
@@ -14,9 +14,6 @@ const saleRoutes = require("./routes/sales");
 
 app.use(cors());
 app.use( express.static('public') );
-app.get('*', (req, res) => {
-  res.sendFile( __dirname + '/public/index.html');
-})
 
 app.use("/user", userRoutes);
 app.use("/role", roleRoutes);
